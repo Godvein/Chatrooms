e5b06f6b3d8f348b49523a5591584099e6bf455e
Ankit Dware <sabindware@gmail.com>
2024-10-21 16:21:05 +0545

    http update

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name = "home"),
-    path('createroom/', views.createRoom, name = "create_room")
+    path('createroom/', views.createRoom, name = "create_room"),
+    path('<int:id>/', views.joinRoom, name = "join_room")
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import rooms
 # Create your views here.
 def home(request):
-    return render(request, "core/home.html")
+    room = rooms.objects.all()
+    return render(request, "core/home.html", {
+        "rooms" : room
+    })
 
 def createRoom(request):
+    if request.method == "POST":
+        room_name = request.POST.get("roomname")
+        room = rooms(name = room_name)
+        room.save()
+        return redirect("home")
     return render(request, "core/createroom.html")
+
+def joinRoom(request, id):
+    room = rooms.objects.get(id = id)
+    return render(request, "core/room.html", {
+        "room" : room
+    })
