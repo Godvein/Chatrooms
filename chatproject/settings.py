995758561209819904429981e90179f38c78cf28
Ankit Dware <sabindware@gmail.com>
2024-10-22 17:10:18 +0545

    redis configuration

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -51,7 +51,11 @@ ASGI_APPLICATION = "chatproject.asgi.application"
 
 CHANNEL_LAYERS = {
     'default':{
-        "BACKEND" : "channels.layers.InMemoryChannelLayer"
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6380)],  
+        },
+
     }
 }
 
